03f6112e16262ff239880902a55fad67024840a2
skulllism <kimkr1617@naver.com>
2016-07-12 15:58:56 +0900

    0.03
    Game
    Scene
    TSBehavior

---
+++ TrafficSafetyVR/Assets/Scripts/TSBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TSBehavior : MonoBehaviour
+{
+    public Game game { private set; get; }
+
+    protected virtual void Awake()
+    {
+        game = Game.Instance;
+    }
+}
