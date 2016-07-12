33dcfd9754ee77e4a04be3e2dcfaee82b857ea0b
Kaonuri <hon454@naver.com>
2016-07-12 19:39:38 +0900

    0.07
    이동 완성

---
+++ TrafficSafetyVR/Assets/_Scripts/CamRot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRot : MonoBehaviour {
+
+    public static CamRot instance;
+
+
+    [SerializeField]
+    Transform CameraObject;
+
+
+
+    public float rotationXMargin = 25;
+    public float rotationYMargin = 25;
+
+    // Use this for initialization
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraObject.localEulerAngles = new Vector3(Remap(Input.mousePosition.y, 0, Screen.height, rotationYMargin, -rotationYMargin),
+                                                    Remap(Input.mousePosition.x, 0, Screen.width, -rotationXMargin, rotationXMargin),
+                                                       0);
+    }
+
+    public static float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
