ad5ffe7be61b1728758e37c54a52237370f05a86
skulllism <kimkr1617@naver.com>
2016-07-18 20:24:29 +0900

    0.0.24

---
+++ TrafficSafetyVR/Assets/_Scripts/TSEventGazeTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TSEventGazeTarget : TSBehavior
+{
+    public bool clear { private set; get; }
+
+    public void Clear()
+    {
+        clear = true;
+    }
+}
