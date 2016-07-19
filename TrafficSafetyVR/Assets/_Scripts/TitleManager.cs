83ae85db1b6d8bccaca834545ba9ec1b692e4eb9
Kaonuri <hon454@naver.com>
2016-07-19 11:04:01 +0900

    0.27
    오늘의 첫 커밋

---
+++ TrafficSafetyVR/Assets/_Scripts/TitleManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+    public Image _logo;
+    public float _sceneChangeTime;
+
+	void Start ()
+	{
+	    _logo.DOFade(0f, 4f).From();
+	    _logo.transform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 4f);
+        Invoke("GoToMain", _sceneChangeTime);
+	}
+
+    private void GoToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
