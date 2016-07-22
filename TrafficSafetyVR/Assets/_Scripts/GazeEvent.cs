a55f7d3c4b0d1af0b7a518c50016f9797b6d3348
skulllism <kimkr1617@naver.com>
2016-07-22 18:54:02 +0900

    0.0.5
    GazeEvent making

---
+++ TrafficSafetyVR/Assets/_Scripts/GazeEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeEvent : TSBehavior
+{
+    public bool complete { private set; get; }
+
+    public void Reset()
+    {
+        complete = false;
+    }
+
+    public void Complete()
+    {
+        complete = true;
+    }
+}
+
--- TrafficSafetyVR/Assets/_Scripts/TrafficLightPedestrian.cs
+++ TrafficSafetyVR/Assets/_Scripts/TrafficLightPedestrian.cs
@@ -3,6 +3,42 @@ using System.Collections;
 
 public class TrafficLightPedestrian : TrafficLight
 {
+    private GazeEvent[] gazeEvents;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        gazeEvents = GetComponentsInChildren<GazeEvent>();
+    }
+
+    public void ActiveGazeEvents()
+    {
+        for (int i = 0; i < gazeEvents.Length; i++)
+        {
+            gazeEvents[i].gameObject.SetActive(true);
+        }
+    }
+
+    public void DisableGazeEvents()
+    {
+        for (int i = 0; i < gazeEvents.Length; i++)
+        {
+            gazeEvents[i].Reset();
+            gazeEvents[i].gameObject.SetActive(false);
+        }
+    }
+
+    public bool IsGazeEventsComplete()
+    {
+        for (int i = 0; i < gazeEvents.Length; i++)
+        {
+            if (!gazeEvents[i].complete)
+                return false;
+        }
+
+        return true;
+    }
+
     public override void SetSign(SignType type)
     {
         base.SetSign(type);
@@ -11,9 +47,12 @@ public class TrafficLightPedestrian : TrafficLight
 
         if (type == SignType.Green)
         {
+            ActiveGazeEvents();
         }
+
         if (type == SignType.Red)
         {
+            DisableGazeEvents();
         }
     }
 }
